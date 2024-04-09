4de2cd83fd9a955374135a7999dac1b86230c5c8
MukulKrSingh <singhmukul96@gmail.com>
2024-04-09 16:33:20 +0530

    Default Mux usecase for handling d/f end points

--- net_http/main.go
+++ net_http/main.go
@@ -5,14 +5,21 @@ import (
 	"net/http"
 )
 
-type hotdog int
-
-func (h hotdog) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
-	fmt.Fprintln(resp, "Anything goes here")
+func d(resp http.ResponseWriter, req *http.Request) {
+	fmt.Fprintln(resp, "Anything goes here :  Dog")
+}
+func c(resp http.ResponseWriter, req *http.Request) {
+	fmt.Fprintln(resp, "Anything goes here : Cat")
+}
+func cat(resp http.ResponseWriter, req *http.Request) {
+	fmt.Fprintln(resp, "Anything goes here : Cat Meows")
 }
 
 func main() {
-	d := hotdog(1)
-	http.ListenAndServe(":8080", d)
+
+	http.HandleFunc("/dog", d)
+	http.HandleFunc("/cat", c)
+	http.HandleFunc("/cat/mewo", cat)
+	http.ListenAndServe(":8080", nil) //http already has Dafault Mux in place and we can use it by passing nil handler of ListenAndServe
 
 }
